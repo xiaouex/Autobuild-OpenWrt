593d4b976f75f0f700611a04533dc5c7269017bc
xiaouex <36121363+xiaouex@users.noreply.github.com>
2026-09-05 16:22:39 +0800

    Update SETUP_rax3000m.sh

--- config/SETUP_rax3000m.sh
+++ config/SETUP_rax3000m.sh
@@ -99,7 +99,7 @@ function task_step_5() {
     
     #Add BORE Scheduler
     git clone -b main https://github.com/firelzrd/bore-scheduler $GITHUB_WORKSPACE/config/files/BORE
-    cp $GITHUB_WORKSPACE/config/files/BORE/patches/stable/linux-6.12-bore/*.patch target/linux/generic/hack-6.18
+    cp $GITHUB_WORKSPACE/config/files/BORE/patches/stable/linux-6.18-bore/*.patch target/linux/generic/hack-6.18
     cp $GITHUB_WORKSPACE/config/files/BORE/patches/additions/*.patch target/linux/generic/pending-6.18
     rm -rf $GITHUB_WORKSPACE/config/files/BORE
 
